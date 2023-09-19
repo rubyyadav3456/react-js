4154e464d1ffa38d6d62f962680d14c4ca9be5e6
Rubycode <rubyruby3423@gmail.com>
2023-09-19 14:26:26 +0530

    first commit

---
+++ awesomeapp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import  ReactDOM from "react-dom";
+
+
+const App = () => {
+  return(
+    <>
+        <h1> Welcome to <span style={ {fontWeight : 'bold'} }> Slot machine game </span> </h1>
+        
+    </>
+  );
+  
+};
+
+
+export default App;
---
+++ awesomeapp/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App"; // Adjust the import path based on your project structure.
+
+ReactDOM.render(
+   <App />,
+ document.getElementById("root")
+ );
+ 
